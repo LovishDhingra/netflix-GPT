dd795c35b5b1d2a0583395aec4e2ebb2dbcbc921
LovishDhingra <lovihdhingra2023@gmail.com>
2024-04-04 23:50:38 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3x1 font-bold text-blue-200">
+   Namaste Everyone lets build chat gpt
+    </div>
+  );
+}
+
+export default App;
